48ed3dda5ca681384c85c1e2e05d6b28c856b131
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-12-13 00:08:58 +0530

    Added: calculated length of a line with points A(x1,y1) and  B(x2,y2)

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
+         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter co-ordinates(x1, y1) of point A of line: ");
+        float x1 = scanner.nextInt();
+        float y1 = scanner.nextInt();
+        System.out.println("Enter co-ordinates(x2, y2) of point B of line: ");
+        float x2 = scanner.nextInt();
+        float y2 = scanner.nextInt();
+
+        float x = (float) Math.pow((x2 - x1),2);
+        float y = (float) Math.pow((y2 - y1), 2);
+        float length = (float) Math.sqrt(x + y);
+        System.out.println("The length of the line = "+length);
     }
 }
